2b9b500a1a8c9b55abcd1c476d8e94560a687b9e
jeristiano <jeremykuang@foxmail.com>
2018-01-15 14:48:15 +0000

    [emailsend] git commit -am[emailsend]

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,11 +14,13 @@ class UsersTableSeeder extends Seeder
 
         $users = factory(User::class)->times(50)->make();
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+
         $user = User::find(1);
         $user->name = 'Aufree';
         $user->email = 'aufree@yousails.com';
         $user->password = bcrypt('password');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
