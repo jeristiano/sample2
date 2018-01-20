d6db3b540f68591772b6b96f4be9ee6f0c64f61c
vagrant <vagrant@homestead>
2018-01-20 15:03:54 +0000

    procfile for heroku

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -9,35 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('/about', 'StaticPagesController@about')->name('about');
 
-//用户注册路由
-
-Route::get('signup', 'UsersController@create')->name('signup');
-Route::resource('users', 'UsersController');
-
-//会话控制
-Route::get('login', 'SessionsController@create')->name('login');
-Route::post('login', 'SessionsController@store')->name('login');
-Route::delete('logout', 'SessionsController@destroy')->name('logout');
-
-//token确认
-Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
-//密码重设
-
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
-
-//微博首页资源路由
-Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);//限制只有更新和删除方法
-
-
-Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
-Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
-
-Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
-Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
+Route::get('/', function () {
+    return view('welcome');
+});
